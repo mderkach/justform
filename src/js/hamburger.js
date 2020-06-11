587b6428edb5c528434f70f4c2e7113e80299d79
mderkach <derkach94@gmail.com>
2020-06-11 13:10:35 +0300

    fix font, typo, header and filter hovers

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,11 +1,11 @@
-const hamburger = document.querySelector('.hamburger');
-// On click
-if (hamburger) {
-  hamburger.addEventListener('click', function () {
-    // Toggle class "is-active"
-    hamburger.classList.toggle('is-active');
-    // Do something else, like open/close menu
-  });
-}
+// const hamburger = document.querySelector('.hamburger');
+// // On click
+// if (hamburger) {
+//   hamburger.addEventListener('click', function () {
+//     // Toggle class "is-active"
+//     hamburger.classList.toggle('is-active');
+//     // Do something else, like open/close menu
+//   });
+// }
 
-console.log('hamb loaded');
+// console.log('hamb loaded');
