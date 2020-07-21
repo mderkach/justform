fc00bf875ea5aae10d118efad085c1fb166fad5d
mderkach <derkach94@gmail.com>
2020-07-21 11:35:11 +0300

    filter simple update value on init

--- src/views/components/filter/filterSimple.js
+++ src/views/components/filter/filterSimple.js
@@ -4,21 +4,26 @@ const filterSimple = {
     if (filterSimple.wrapper) {
       filterSimple.wrapper.forEach((filter) => {
         const trigger = filter.querySelector('.filter-simple-value');
-
         const items = filter.querySelectorAll('.filter-simple-label');
-        trigger.addEventListener('click', (e) => {
-          e.preventDefault();
-          trigger.parentElement.classList.toggle('is-active');
-        });
-
-        items.forEach((item) => {
-          item.addEventListener('click', () => {
-            const input = item.querySelector('input');
-            const text = input.value;
-            trigger.textContent = text;
-            trigger.parentElement.classList.remove('is-active');
+        if (trigger) {
+          trigger.addEventListener('click', (e) => {
+            e.preventDefault();
+            trigger.parentElement.classList.toggle('is-active');
           });
-        });
+
+          items.forEach((item) => {
+            const radio = item.querySelector('input');
+            if (radio.hasAttribute('checked')) {
+              trigger.textContent = radio.value;
+            }
+            item.addEventListener('click', () => {
+              const input = item.querySelector('input');
+              const text = input.value;
+              trigger.textContent = text;
+              trigger.parentElement.classList.remove('is-active');
+            });
+          });
+        }
       });
     }
   },
