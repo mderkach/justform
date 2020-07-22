0d1a3bc3d190a7d7de90763aceef7bd6096a4264
mderkach <derkach94@gmail.com>
2020-07-22 12:45:01 +0300

    filter simple fix + placeholder

--- src/views/components/filter/filterSimple.js
+++ src/views/components/filter/filterSimple.js
@@ -13,13 +13,27 @@ const filterSimple = {
 
           items.forEach((item) => {
             const radio = item.querySelector('input');
+
             if (radio.hasAttribute('checked')) {
               trigger.textContent = radio.value;
             }
+
+            if (radio.hasAttribute('disabled')) {
+              radio.parentElement.style.display = 'none';
+              trigger.classList.add('is-placeholder');
+            }
+
             item.addEventListener('click', () => {
               const input = item.querySelector('input');
               const text = input.value;
+              const disabled = input.hasAttribute('disabled');
+
               trigger.textContent = text;
+
+              if (!disabled) {
+                trigger.classList.remove('is-placeholder');
+              }
+
               trigger.parentElement.classList.remove('is-active');
             });
           });
