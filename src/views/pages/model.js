305c7a09bb685b88c7c3650c9a72769811d827d9
Skeev_ <derkach94@gmail.com>
2020-08-11 20:18:56 +0300

    include modal to model page

--- src/views/pages/model.js
+++ src/views/pages/model.js
@@ -3,7 +3,9 @@ import './model.scss';
 // js & components
 import accordion from '../components/accordion/accordion';
 import model from '../modules/model/model';
+import modal from '../../js/openModal';
 
 // initialise components
 accordion.init();
 model.init();
+modal.init();
