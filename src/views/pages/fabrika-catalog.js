cd5441c2c7d8d0aed66c55ae7e3befc96815420b
skeev_ <derkach94@gmail.com>
2020-09-01 10:36:50 +0300

    fabrika page init

---
+++ src/views/pages/fabrika-catalog.js
@@ -0,0 +1,7 @@
+import './fabrika-catalog.scss';
+
+import filter from '../components/filter/filter';
+import filterSimple from '../components/filter/filterSimple';
+
+filter.init();
+filterSimple.init();
