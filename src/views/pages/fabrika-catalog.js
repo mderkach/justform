f859c8f20774713f900675509aff882d1d3c2087
skeev_ <derkach94@gmail.com>
2020-09-01 11:42:42 +0300

    fabrika catalog finish

--- src/views/pages/fabrika-catalog.js
+++ src/views/pages/fabrika-catalog.js
@@ -2,6 +2,8 @@ import './fabrika-catalog.scss';
 
 import filter from '../components/filter/filter';
 import filterSimple from '../components/filter/filterSimple';
+import modal from '../../js/openModal';
 
 filter.init();
 filterSimple.init();
+modal.init();
