b84b53aff702afb302af99330a62ac7dc29fdbb3
mderkach <derkach94@gmail.com>
2020-10-01 15:09:24 +0300

    accordion fix

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -5,8 +5,10 @@ import './index.scss';
 import sliderBanner from '../components/slider/sliderBanner';
 import cardGray from '../components/card/cardGray';
 import itemLink from '../components/item/itemLink';
+import accordion from '../components/accordion/accordion';
 
 // initialise components
 sliderBanner.init();
 cardGray.init();
 itemLink.init();
+accordion.init();
