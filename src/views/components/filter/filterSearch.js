c819f815756af48ad19918cba3881ce95de5e454
skeev_ <derkach94@gmail.com>
2020-10-24 14:37:29 +0300

    filter search test

---
+++ src/views/components/filter/filterSearch.js
@@ -0,0 +1,43 @@
+import './filterSearch.scss';
+
+const filterSearch = {
+  wrapper: document.querySelectorAll('.filter-search-wrapper'),
+  onInput(wrapper) {
+    const input = wrapper.querySelector('input');
+
+    input.addEventListener('keyup', (e) => {
+      if (e.isComposing || e.keyCode === 229) {
+        return;
+      }
+
+      setTimeout(() => {
+        if (input.value.length >= 3) {
+          alert(`Идет поиск по запросу: ${input.value}.`);
+        }
+      }, 500);
+    });
+  },
+  openFilter: (elm) => {
+    const header = elm.querySelector('.filter-search-header');
+
+    header.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (elm.classList.contains('is-active')) {
+        elm.classList.remove('is-active');
+      } else {
+        elm.classList.add('is-active');
+      }
+    });
+  },
+  init: () => {
+    if (filterSearch.wrapper.length > 0) {
+      filterSearch.wrapper.forEach((wrapper) => {
+        filterSearch.openFilter(wrapper);
+        filterSearch.onInput(wrapper);
+      });
+    }
+  },
+};
+
+export default filterSearch;
--- src/views/pages/catalog.js
+++ src/views/pages/catalog.js
@@ -5,7 +5,9 @@ import './catalog.scss';
 import filter from '../components/filter/filter';
 import filterSimple from '../components/filter/filterSimple';
 import filterReset from '../components/filter/filterReset';
+import filterSearch from '../components/filter/filterSearch';
 // initialise components
 filter.init();
 filterSimple.init();
 filterReset.init();
+filterSearch.init();
