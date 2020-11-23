718ce89c26ec2e10f0f911f1ae8f96b95795b225
turkish joe <turkishjoe283@gmail.com>
2020-11-23 10:51:23 +0300

    fix accordion

--- src/views/components/accordion/accordion.js
+++ src/views/components/accordion/accordion.js
@@ -6,14 +6,16 @@ const accordion = {
     if (accordion.items) {
       accordion.items.forEach((item) => {
         const header = item.querySelector('.accordion__header');
-        header.addEventListener('click', (e) => {
-          e.preventDefault();
-          if (header.parentElement.classList.contains('is-active')) {
-            header.parentElement.classList.remove('is-active');
-          } else {
-            header.parentElement.classList.add('is-active');
-          }
-        });
+        if(header) {
+          header.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (header.parentElement.classList.contains('is-active')) {
+              header.parentElement.classList.remove('is-active');
+            } else {
+              header.parentElement.classList.add('is-active');
+            }
+          });
+        }
       });
     }
   },
