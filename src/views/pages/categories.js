fc341fceeaeaaff65e77a42fbfd96a99fff239f6
skeev_ <derkach94@gmail.com>
2020-12-01 15:36:28 +0300

    categoies, contacts fix

---
+++ src/views/pages/categories.js
@@ -0,0 +1,16 @@
+// styles
+import './categories.scss';
+
+// js & components
+import sliderBanner from '../components/slider/sliderBanner';
+import itemLink from '../components/item/itemLink';
+import accordion from '../components/accordion/accordion';
+import filter from '../components/filter/filter';
+import cardGray from '../components/card/cardGray';
+
+// initialise components
+sliderBanner.init();
+itemLink.init();
+accordion.init();
+filter.init();
+cardGray.init();
